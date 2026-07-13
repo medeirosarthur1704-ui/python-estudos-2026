b7552bce36e4bdd18c41bbe1dade8513ea509101
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-13 13:36:40 -0300

    Update calculadora.py

--- projetos/calculadora/calculadora.py
+++ projetos/calculadora/calculadora.py
@@ -1,23 +1,33 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o Segundo número: "))
+num2 = int(input("Digite o segundo número: "))
 operação = input("Digite a operação: ")
 
 match operação:
     case "+":
         resultado = num1 + num2
+        resultado = f"{num1 + num2}✅"
     case "-":
         resultado = num1 - num2
+        resultado = f"{num1 - num2}✅"
     case "*":
         resultado = num1 * num2
     case "/": 
         resultado = num1 / num2
+        resultado = f"{num1 * num2}✅"
+    case "/":
+        resultado = f"{num1 / num2}✅"
     case "**":
         resultado = num1 ** num2
+        resultado = f"{num1 ** num2}✅"
     case "//":
         resultado = num1 // num2
+        resultado = f"{num1 // num2}✅"
     case "%":
         resultado = num1 % num2
+        resultado = f"{num1 % num2}✅"
     case _:
         resultado = "Operação inválida❌"
 
+        
 print(f"O resultado da operação é: {resultado}")
