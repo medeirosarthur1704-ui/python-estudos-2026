f3dbb1b8896c1d4972c9663f99be0308a2b51c81
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-17 15:59:43 -0300

    Update calculadora.py

--- projetos/calculadora/calculadora.py
+++ projetos/calculadora/calculadora.py
@@ -1,23 +1,29 @@
-n1 = int(input("Digite o Primeiro Número: "))
-n2 = int(input("Digite o Segundo Número: "))
-operação = input("Digite a Operação: ")
+try:
+    n1 = int(input("DIGITE O PRIMEIRO NÚMERO: "))
+    n2 = int(input("DIGITE O SEGUNDO NÚMERO: "))
+    operação = input("DIGITE A OPERAÇÃO: ")
 
-match operação:
-    case "+":
-        resultado = f"{n1 + n2}✅"
-    case "-":
-        resultado =  f"{n1 - n2}✅"
-    case "*":
-        resultado = f"{n1 * n2}✅"
-    case "/":
-        resultado = f"{n1 / n2}✅"
-    case "**":
-        resultado = f"{n1 ** n2}✅"
-    case "//":
-        resultado = f"{n1 // n2}✅"
-    case "%":
-        resultado = f"{n1 % n2}✅"
-    case _:
-        resultado = "Operação Inválida❌"
-        
-print(f"O Resultado da Operação é: {resultado}")
+    match operação:
+        case "+":
+            resultado = n1 + n2
+        case "-":
+            resultado = n1 - n2
+        case "*":
+            resultado = n1 * n2
+        case "/":
+            resultado = n1 / n2
+        case "**":
+            resultado = n1 ** n2
+        case "//":
+            resultado = n1 // n2
+        case "%":
+            resultado = n1 % n2
+        case _:
+            resultado = "OPERAÇÃO INVÁLIDA"
+
+    print(f"O RESULTADO DA OPERAÇÃO É: {resultado}")
+
+except ValueError:
+    print("DIGITE APENAS NÚMEROS.")
+except ZeroDivisionError:
+    print("NÃO É POSSÍVEL DIVIDIR POR ZERO.")
