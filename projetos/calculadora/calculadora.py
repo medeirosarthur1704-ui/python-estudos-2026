d73e49ca24d7ef1bb8ad458bbed59c1c3cd9a657
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-18 13:09:04 -0300

    Update calculadora.py

--- projetos/calculadora/calculadora.py
+++ projetos/calculadora/calculadora.py
@@ -12,11 +12,11 @@ try:
         case "/":
             resultado = f"{n1 / n2:.2f}✅"
         case "**":
-            resultado = f"{n1 ** n2}✅"
+            resultado = f"{n1 ** n2:.2f}✅"
         case "//":
-            resultado = f"{n1 // n2}✅"
+            resultado = f"{n1 // n2:.0f}✅"
         case "%":
-            resultado = f"{n1 % n2}✅"
+            resultado = f"{n1 % n2:.2f}✅"
         case _:
             resultado = "❌ Operação inválida"
     
@@ -25,5 +25,4 @@ except ValueError:
     print("⚠️  Erro: Digite apenas números!")
 except ZeroDivisionError:
     print("⚠️  Erro: Não é possivel dividir por zero.")
-    
     
