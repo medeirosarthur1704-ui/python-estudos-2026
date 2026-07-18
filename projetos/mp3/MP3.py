aac8bfa5fe5540e4c3ca70c92dfe1e7517a706cf
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-18 17:42:29 -0300

    Apliquei comandos de pause

---
+++ projetos/mp3/MP3.py
@@ -0,0 +1,21 @@
+import pygame
+import time
+
+pygame.mixer.init()
+
+nome_arquivo = input("Qual música você quer tocar? (nome do arquivo): ")
+pygame.mixer.music.load(nome_arquivo)
+pygame.mixer.music.play()
+
+while True:
+    comando = input("Digite (p) pausar, (c) continuar, (s) sair: ")
+    
+    if comando == "p":
+        pygame.mixer.music.pause()
+    elif comando == "c":
+        pygame.mixer.music.unpause()
+    elif comando == "s":
+        pygame.mixer.music.stop()
+        break
+    else:
+        print("Comando inválido!")
