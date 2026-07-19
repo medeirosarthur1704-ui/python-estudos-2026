d6e1146934ddbfbf18e9908813618945d8abaede
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-18 23:13:47 -0300

    Update calculadora.py

--- projetos/calculadora/calculadora.py
+++ projetos/calculadora/calculadora.py
@@ -1,7 +1,7 @@
 try:
     n1 = float(input("Digite o primeiro número: "))
     n2 = float(input("Digite o segundo número: "))
-    operação = input("Digite a operação: ")
+    operação = input("Digite a operação (+, -, *, /, **, //, %): ")
     match operação:
         case "+":
             resultado = f"{n1 + n2:.0f}✅"
@@ -18,7 +18,7 @@ try:
         case "%":
             resultado = f"{n1 % n2:.2f}✅"
         case _:
-            resultado = "❌ Operação inválida"
+            resultado = "Operação inválida ❌"
     
     print(f"O resultado da operação é: {resultado}")
 except ValueError:
