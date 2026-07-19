cda68c1ab316823f99c068d8cb5bc85fe89b7f61
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-07-18 23:21:46 -0300

    Update MP3.py

--- projetos/mp3/MP3.py
+++ projetos/mp3/MP3.py
@@ -3,19 +3,22 @@ import time
 
 pygame.mixer.init()
 
-nome_arquivo = input("Qual música você quer tocar? (nome do arquivo): ")
-pygame.mixer.music.load(nome_arquivo)
-pygame.mixer.music.play()
+try:
+    nome_arquivo = input("Qual música você quer tocar? (nome do arquivo): ")
+    pygame.mixer.music.load(nome_arquivo)
+    pygame.mixer.music.play()
 
-while True:
-    comando = input("Digite (p) pausar, (c) continuar, (s) sair: ")
-    
-    if comando == "p":
-        pygame.mixer.music.pause()
-    elif comando == "c":
-        pygame.mixer.music.unpause()
-    elif comando == "s":
-        pygame.mixer.music.stop()
-        break
-    else:
-        print("Comando inválido!")
+    while True:
+        comando = input("Digite (p) pausar, (c) continuar, (s) sair: ")
+        if comando == "p":
+            pygame.mixer.music.pause()
+        elif comando == "c":
+            pygame.mixer.music.unpause()
+        elif comando == "s":
+            pygame.mixer.music.stop()
+            break
+        else:
+            print("Comando inválido!")
+
+except pygame.error:
+    print("⚠️  Erro: Arquivo não encontrado! Verifique o nome digitado.")
