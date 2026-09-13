67a820ecc8649df05dd454fdefb224373f282abd
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-09-13 15:45:21 -0300

    ...

--- projetos/Senhas ocultas/Senhas ocultas.py
+++
@@ -1,8 +0,0 @@
-import pwinput
-
-senha = pwinput.pwinput("Digite sua senha: ", mask = "*")
-
-senha_oculta = "*" * len(senha)
-
-print(f"Senha oculta: {senha_oculta}")
-print("✅ Senha recebida com sucesso!")
