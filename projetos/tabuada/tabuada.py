c9d3d299f7e43efc02e3a574fc797d30623821e6
medeirosarthur1704-ui <medeiros.arthur1704@gmail.com>
2026-09-16 00:21:57 -0300

    ...

--- projetos/tabuada/tabuada.py
+++ projetos/tabuada/tabuada.py
@@ -6,7 +6,7 @@
 import pyfiglet          # gera o banner em ASCII art
 
 # Banner de título, em vermelho vivo
-banner = pyfiglet.figlet_format("Tabuada")
+banner = pyfiglet.figlet_format("Tabuada", font="slant")
 print(f"\033[1;38;2;255;40;40m{banner}\n")
 
 # Pede o número que o usuário quer ver a tabuada
@@ -14,4 +14,4 @@ num = int(input("\033[1;34m\n[+] Digite um número: "))
 
 # Percorre de 1 até 10 (range(1, 11) exclui o 11) multiplicando 'num' por cada 'i'
 for i in range(1, 11):
-    print(f"\033[1;35m{num} x {i} = {num * i}")
+    print(f"\033[1;35m{num} x {i} = \033[1;33m{num * i}\033[0m")
